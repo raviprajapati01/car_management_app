609f0e3c3006dae339b37b07cfebd2b534bbc488
raviprajapati01 <145858419+raviprajapati01@users.noreply.github.com>
2024-11-15 02:18:52 +0530

    Update RegisterPage.jsx

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
 import "../styles/Register.scss";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001"
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://car-management-app-2cwb.onrender.com"
 
 
 const RegisterPage = () => {
