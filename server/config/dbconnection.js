c747e0442d4e071cf9e9dc67cb24e6555d07bf47
raviprajapati01 <145858419+raviprajapati01@users.noreply.github.com>
2024-11-15 03:23:26 +0530

    Update dbconnection.js

--- server/config/dbconnection.js
+++ server/config/dbconnection.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 
 exports.dbconnection = () => {
     const PORT = process.env.PORT || 3001;
-    mongoose.connect(process.env.MONGO_URL || "mongodb+srv://raviprajapatikandari:MjpkdMQkSUZSCRcm@cluster0.5denj.mongodb.net/DreamHaven")
+    mongoose.connect(process.env.MONGO_URL || "mongodb+srv://raviprajapatikandari:NCTcGgh0IjUGHsiO@cluster0.s0qkx.mongodb.net/Car_Management?retryWrites=true&w=majority&appName=Cluster0")
         .then(() => {
         console.log(`Server Start successfully port no : ${PORT}`);
         })
