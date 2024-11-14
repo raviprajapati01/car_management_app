50f9246134738f743895fef4cf2d161fb7cd9cdc
raviprajapati01 <145858419+raviprajapati01@users.noreply.github.com>
2024-11-15 02:15:52 +0530

    Update Listings.jsx

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setListings } from "../redux/state";
 
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001"
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://car-management-app-2cwb.onrender.com"
 
 const Listings = () => {
   const dispatch = useDispatch();
