8e9c4a46f32babbbf89dc982413dc2c2b21a1024
raviprajapati01 <145858419+raviprajapati01@users.noreply.github.com>
2024-11-15 14:54:25 +0530

    Update ReservationList.jsx

--- src/pages/ReservationList.jsx
+++ src/pages/ReservationList.jsx
@@ -1,69 +1,69 @@
-import { useEffect, useState } from "react";
-import "../styles/List.scss";
-import Loader from "../components/Loader";
-import Navbar from "../components/Navbar";
-import { useDispatch, useSelector } from "react-redux";
-import { setReservationList } from "../redux/state";
-import ListingCard from "../components/ListingCard";
-import Footer from "../components/Footer"
+// import { useEffect, useState } from "react";
+// import "../styles/List.scss";
+// import Loader from "../components/Loader";
+// import Navbar from "../components/Navbar";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setReservationList } from "../redux/state";
+// import ListingCard from "../components/ListingCard";
+// import Footer from "../components/Footer"
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://car-management-app-172y.onrender.com"
+// const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://car-management-app-172y.onrender.com"
 
 
 const ReservationList = () => {
-  const [loading, setLoading] = useState(true);
-  const userId = useSelector((state) => state.user._id);
-  const reservationList = useSelector((state) => state.user.reservationList);
+//   const [loading, setLoading] = useState(true);
+//   const userId = useSelector((state) => state.user._id);
+//   const reservationList = useSelector((state) => state.user.reservationList);
 
-  const dispatch = useDispatch();
+//   const dispatch = useDispatch();
 
-  const getReservationList = async () => {
-    try {
-      const response = await fetch(
-        `${BACKEND_URL}/users/${userId}/reservations`,
-        {
-          method: "GET",
-        }
-      );
+//   const getReservationList = async () => {
+//     try {
+//       const response = await fetch(
+//         `${BACKEND_URL}/users/${userId}/reservations`,
+//         {
+//           method: "GET",
+//         }
+//       );
 
-      const data = await response.json();
-      dispatch(setReservationList(data));
-      setLoading(false);
-    } catch (err) {
-      console.log("Fetch Reservation List failed!", err.message);
-    }
-  };
+//       const data = await response.json();
+//       dispatch(setReservationList(data));
+//       setLoading(false);
+//     } catch (err) {
+//       console.log("Fetch Reservation List failed!", err.message);
+//     }
+//   };
 
-  useEffect(() => {
-    getReservationList();
-  }, []);
+//   useEffect(() => {
+//     getReservationList();
+//   }, []);
 
-  return loading ? (
-    <Loader />
-  ) : (
-    <>
-      <Navbar />
-      <h1 className="title-list">Your Reservation List</h1>
-      <div className="list">
-        {reservationList?.map(({ listingId, hostId, startDate, endDate, totalPrice, booking=true }) => (
-          <ListingCard
-            listingId={listingId._id}
-            creator={hostId._id}
-            listingPhotoPaths={listingId.listingPhotoPaths}
-            city={listingId.city}
-            province={listingId.province}
-            country={listingId.country}
-            category={listingId.category}
-            startDate={startDate}
-            endDate={endDate}
-            totalPrice={totalPrice}
-            booking={booking}
-          />
-        ))}
-      </div>
-      <Footer />
-    </>
-  );
+//   return loading ? (
+//     <Loader />
+//   ) : (
+//     <>
+//       <Navbar />
+//       <h1 className="title-list">Your Reservation List</h1>
+//       <div className="list">
+//         {reservationList?.map(({ listingId, hostId, startDate, endDate, totalPrice, booking=true }) => (
+//           <ListingCard
+//             listingId={listingId._id}
+//             creator={hostId._id}
+//             listingPhotoPaths={listingId.listingPhotoPaths}
+//             city={listingId.city}
+//             province={listingId.province}
+//             country={listingId.country}
+//             category={listingId.category}
+//             startDate={startDate}
+//             endDate={endDate}
+//             totalPrice={totalPrice}
+//             booking={booking}
+//           />
+//         ))}
+//       </div>
+//       <Footer />
+//     </>
+//   );
 };
 
 export default ReservationList;
